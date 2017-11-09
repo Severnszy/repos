617f8b2570eb918b9f3ffd80bd243db809bfcb07
medoric <33443169+medoric@users.noreply.github.com>
2017-11-09 11:51:49 +0800

    CodeRunner v1.0.0.0

---
+++ code_runner.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void show_info();
+
+int main()
+{
+}
+
+void show_info()
+{
+	cout
+		<< "CodeRunner\n"
+		<< "Version 1.0.0.0\n"
+		<< "Copyright (c) 2017 Medoric\n\n";
+	cout
+		<< "Note:\n"
+		<< "This program should be used together with CodeJudge. While the\n"
+		<< "Judge judges programs, this program generates the output data by\n"
+		<< "running the standard program.\n";
+}
+
